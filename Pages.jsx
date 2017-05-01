24cc8820a37a1bac2d03156cca50cb2d313c8b6c
Jose Maria Toscano <tuertilindo@gmail.com>
2017-05-01 18:45:00 -0300

    add ids pages

--- Pages.jsx
+++ Pages.jsx
@@ -7,6 +7,9 @@ export default class Page extends React.Component {
     for (var key in this.props.pages) {
       pages.push(<Route path={'/' + key} component={this.props.pages[key]} />)
     }
+    for (var keyi in this.props.ids) {
+      pages.push(<Route path={'/' + keyi + '/:id'} component={this.props.pages[keyi]} />)
+    }
     return (<div>
       <Router history={hashHistory}>
         <Route path="/" component={Home} pages={this.props.pages}>
